f8cf0cb42133679e6819bdf8b1f11f550780275b
choieunsong <greatgelly96@gmail.com>
2021-07-07 17:45:44 +0900

    mockMvc로 EventControllerTests post, response 받기

--- src/main/java/com/demo/restapi/events/Event.java
+++ src/main/java/com/demo/restapi/events/Event.java
@@ -1,6 +1,9 @@
 package com.demo.restapi.events;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 @Builder @AllArgsConstructor @NoArgsConstructor
@@ -9,9 +12,13 @@ public class Event {
     private Integer id;
     private String name;
     private String description;
+    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime beginEnrollmentDateTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime closeEnrollmentDateTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime beginEventDateTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime endEventDateTime;
     private String location; // (optional) 이게 없으면 온라인 모임
     private int basePrice; // (optional)
--- src/test/java/com/demo/restapi/events/EventControllerTests.java
+++ src/test/java/com/demo/restapi/events/EventControllerTests.java
@@ -1,5 +1,8 @@
 package com.demo.restapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,22 +11,51 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
 public class EventControllerTests {
     @Autowired
-    MockMvc mockMvc;
+    private WebApplicationContext wac;
 
+    private MockMvc mockMvc;
+    @Before
+    public void setup(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
     @Test
     public void createEvent() throws Exception{
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+//                .beginEnrollmentDateTime(LocalDateTime.of(2021,7,7,16,9,10))
+//                .closeEnrollmentDateTime(LocalDateTime.of(2021,7,8,16,9,10))
+//                .beginEventDateTime(LocalDateTime.of(2021,7,9,16,9,10))
+//                .endEventDateTime(LocalDateTime.of(2021,7,10,16,9,10))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
         mockMvc.perform(post("/api/events/")
-                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaTypes.HAL_JSON)
-                )
-                .andExpect(status().isCreated());
+                .content(new ObjectMapper().registerModule(new Jdk8Module()).writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
