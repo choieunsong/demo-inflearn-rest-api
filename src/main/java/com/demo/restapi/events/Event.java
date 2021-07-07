794b652894f7ea6876cd328b7c44d6f043b29516
choieunsong <greatgelly96@gmail.com>
2021-07-07 19:16:17 +0900

    polishing

--- src/main/java/com/demo/restapi/events/Event.java
+++ src/main/java/com/demo/restapi/events/Event.java
@@ -1,24 +1,20 @@
 package com.demo.restapi.events;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
-    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime beginEnrollmentDateTime;
-    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime closeEnrollmentDateTime;
-    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime beginEventDateTime;
-    @DateTimeFormat(pattern = "yyyy-MM-dd kk:mm:ss")
     private LocalDateTime endEventDateTime;
     private String location; // (optional) 이게 없으면 온라인 모임
     private int basePrice; // (optional)
@@ -26,6 +22,7 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 
 }
